0d17306725c9b657f2301ee12d5448054f9e7e28
AbdurrohmanJCom <tuckerticket@gmail.com>
2025-04-15 19:50:38 +0800

    some problems with apps fixed

--- static/static/settings.py
+++ static/static/settings.py
@@ -41,6 +41,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'courses',
+    'testimonials',
+    'appointments',
+    'contacts',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -77,21 +84,13 @@ WSGI_APPLICATION = 'static.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'silkwayglobal',
-    #     'USER': 'postgres',
-    #     'PASSWORD': '@Grandtalim1',
-    #     'HOST': 'localhost',
-    #     'PORT': '5432',
-    # }
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
+        'USER': os.getenv("DB_USER", "postgres"),
         'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT"),
+        'HOST': os.getenv("DB_HOST", "localhost"),
+        'PORT': os.getenv("DB_PORT", "5432"),
       
     }
 }
